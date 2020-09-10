fe2550aa39f6bbd654e9707e2c3649b4ba23d460
Matheus Maldonado da Silva <matheussilvacontat@gmail.com>
2020-09-10 12:56:04 -0300

    Criando projeto localizacaixa

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Localiza caixa</h1>
+      <span className="badge badge-primary">Custom</span>
+    </div>
+  );
+}
+
+export default App;
